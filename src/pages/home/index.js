bc6141e965dd3b6b59a28d6ad5fbee9fcc637c40
Umesh <umesh.sh31@gmail.com>
2022-05-10 00:03:41 -0400

    added about button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -48,6 +48,14 @@ export const Home = () => {
                       <div className="ring three"></div>
                     </div>
                   </Link>
+                  <Link to="/about" className="text_2">
+                    <div id="button_p" className="ac_btn btn ">
+                      About Me
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
+                  </Link>
                   <Link to="/contact">
                     <div id="button_h" className="ac_btn btn">
                       Contact Me
